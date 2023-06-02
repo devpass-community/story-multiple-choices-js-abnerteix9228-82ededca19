d56e927dfbfd783f87c7deb6036fcf04220909cd
Abner Teixeira <64090571+Bnerz@users.noreply.github.com>
2023-06-03 00:51:08 +0100

    The chosen test is done

--- chooseProduct.js
+++ chooseProduct.js
@@ -1,5 +1,24 @@
 function chooseProduct(segment) {
     // Add your solution here!
+    switch (segment) {
+        case "Beverage":
+            return "Soda"
+
+            break;
+
+        case "Fruit":
+            return "Apple"
+
+            break;
+
+        case "Hygiene":
+            return "Soap"
+
+            break;
+
+        default:
+            break;
+    }
 }
 
 module.exports = chooseProduct;
